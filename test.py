f2fe65583bab7d529ada9342fcdae3e9a9ab72d4
Moe Far <mhipo1364@gmail.com>
2016-05-09 13:35:51 +0430

    add test module to check divisible

---
+++ test.py
@@ -0,0 +1,52 @@
+def is_divisible(x, y):
+    """
+    Check if x is divided by y
+    :param x: int
+    :param y: int
+    :return: bool
+    """
+    if x % y == 0:
+            return True
+
+
+def divisible_by_three(x):
+    """
+    Returns proper string if x is divided by 3
+    :param x: int
+    :return: str/None
+    """
+    if is_divisible(x, 3):
+        if is_divisible(x, 5):
+            return '{} Big Bang \n'.format(x)
+        return '{} Big \n'.format(x)
+
+
+def divisible_by_five(x):
+    """
+    Returns proper string if x is divided by 5
+    :param x: int
+    :return: str/None
+    """
+    if is_divisible(x, 5):
+        if is_divisible(x, 3):
+            return '{} Big Bang \n'.format(x)
+        return '{} Bang \n'.format(x)
+
+
+def main(x):
+    """
+    Decision Maker
+    :param x: int
+    :return: str/None
+    """
+    result = divisible_by_three(x)
+    if not result:
+        result = divisible_by_five(x)
+    return result
+
+
+start = 4
+end = 146
+
+for i in range(start, end):
+    print(main(i))
